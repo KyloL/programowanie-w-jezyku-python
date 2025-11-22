5154ed4f854745d62d92ec74c5c0a30a1648b9e3
Krzysztof Lisak <lisak_k@poczta.onet.pl>
2025-11-22 17:07:53 +0100

    python_typing

--- zad2a.py
+++
@@ -1,4 +0,0 @@
-def print_names (names):
-    for n in names:
-        print(n)
-print_names(['ala', 'as', 'pas'])
--- zad2c.py
+++
@@ -1,5 +0,0 @@
-def iven(liczby):
-    for i in range(10):
-        if i % 2 == 0:
-            print(i)
-iven([1,0,9,4,6,7,24,6,46,12])
