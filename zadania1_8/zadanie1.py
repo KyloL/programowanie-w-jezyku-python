43cf603d23c5f1c3b600ca33c6463bde4ba49bf3
Krzysztof Lisak <lisak_k@poczta.onet.pl>
2025-11-22 17:10:14 +0100

    python_basic

--- zadania1_8/zadanie1.py
+++
@@ -1,6 +0,0 @@
-def hello (name: str, sname: str) -> str:
-    return 'Cześć ' + name + " " + sname + '!'
-
-helloname = hello ('Kylo', 'Lisak')
-
-print(helloname)
--- zadania1_8/zadanie4.py
+++
@@ -1,4 +0,0 @@
-def sprawdz(a:int,b:int,c:int)->bool:
-    return (a+b) > c
-
-print(sprawdz(1,2,3))
